d422d4d1c3ad732f5cf7ec23cfe7ca603ca95617
lambertkasaija <lambertkasaija@gmail.com>
2024-09-13 14:57:23 +0300

    commit

--- flight_booking/app/views.py
+++ flight_booking/app/views.py
@@ -1,2 +1,20 @@
-from django.shortcuts import render
-from 
+from django.shortcuts import render,redirect
+from .models import FlightPassenger
+from .forms import FlightPassengerForm
+from django.contrib import messages
+
+
+# Create your views here.
+def add_booking(request):
+    if request.method == 'POST':
+        form = FlightPassengerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Form submitted successfully!')
+            return redirect('/')
+        else:
+            messages.error(request, 'There was an error with your submission.')
+    else:
+        form = FlightPassengerForm()
+
+    return render(request, 'booking/add_booking.html', {'form':form})
